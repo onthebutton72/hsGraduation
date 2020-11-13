f54f5c4be0bb80f50838de99275bf4fbe7a629e3
Jamie Martinez <martinezjamie@hotmail.com>
2020-11-13 14:33:12 -0800

    mid sized responsive design

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,27 @@
-var background = 6;
-console.log(background);
+const myClass = "Class of 2021";
+document.getElementById("myClass").innerHTML = myClass;
+document.getElementById("myClass-ls").innerHTML = myClass;
+
+const myAnnoucement = "We joyfully announce the graduation of...";
+document.getElementById("myAnnoucement").innerHTML = myAnnoucement;
+document.getElementById("myAnnoucement-ls").innerHTML = myAnnoucement;
+
+const myName = "Anna Sophia Martinez";
+document.getElementById("myName").innerHTML = myName;
+document.getElementById("myName-ls").innerHTML = myName;
+
+const mySchool = "Casa Roble High School";
+document.getElementById("mySchool").innerHTML = mySchool;
+document.getElementById("mySchool-ls").innerHTML = mySchool;
+
+const myGradDate = "June 22, 2021";
+document.getElementById("myGradDate").innerHTML = myGradDate;
+document.getElementById("myGradDate-ls").innerHTML = myGradDate;
+
+const myQuote = "To exist is to change, to change is to mature, to mature is to go on creating oneself endlessly.";
+document.getElementById("myQuote").innerHTML = myQuote;
+document.getElementById("myQuote-ls").innerHTML = myQuote;
+
+const myAuthor = "~ Henri Bergson";
+document.getElementById("myAuthor").innerHTML = myAuthor;
+document.getElementById("myAuthor-ls").innerHTML = myAuthor;
